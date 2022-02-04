3aa4f6d6d074f453c732eaac38741988ef994b8e
Juan Ayala <juayadel@gmail.com>
2022-02-04 12:22:24 -0500

    commit proyect EF

---
+++ CreateBd/Weelo.App.Api.EF/DataModel/Entities/Owner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Weelo.App.Api.EF.DataModel
+{
+    public class Owner
+    {
+        [Key]
+        [Required]
+        public int? OwnerId { get; set; }
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Address { get; set; }
+        public byte[] Photo { get; set; }
+        [StringLength(256)]
+        public string PhotoName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int? PropertyId { get; set; }
+    }
+}
---
+++ CreateBd/Weelo.App.Api.EF/DataModel/Entities/Property.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Weelo.App.Api.EF.DataModel
+{
+    [Index(nameof(Year))]
+    public class Property
+    {
+        [Key]
+        [Required]
+        public int? PropertyId { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Address { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public int CodeInternal { get; set; }
+        public int Year { get; set; }
+        public List<Owner> Owners { get; set; }
+        public List<PropertyImage> Images { get; set; }
+
+    }
+}
